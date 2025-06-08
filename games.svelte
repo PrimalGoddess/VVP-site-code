19e2d32f1b5fe38246e5948e17b3ffe7549b254b
PrimalGoddess <131571808+PrimalGoddess@users.noreply.github.com>
2025-06-07 21:35:48 -0400

    Create games.svelte

---
+++ games.svelte
@@ -0,0 +1,15 @@
+<script>
+        import { visibleComponent } from "./store";
+        const buttonPageHigherLower = () => {
+        // Open the higher lower game
+        $visibleComponent = "higherLower";
+    };
+
+        const buttonPageSudoku = () => {
+        // Open the sudoku game
+        $visibleComponent = "sudoku";
+    };
+</script>
+
+<button on:click={buttonPageHigherLower}>Play Higher or Lower</button>
+<button on:click={buttonPageSudoku}>Play Sudoku</button>
