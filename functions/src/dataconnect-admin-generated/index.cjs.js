bcc582edd0762fa40dfa6a99df93977f70b2dd5d
aparthenopaeus94 <aparthenopaeus94@gmail.com>
2025-11-24 02:04:06 -0600

    fixed firebase code

--- functions/src/dataconnect-admin-generated/index.cjs.js
+++ functions/src/dataconnect-admin-generated/index.cjs.js
@@ -1,37 +1,42 @@
-const { validateAdminArgs } = require('firebase-admin/data-connect');
+import { validateAdminArgs } from 'firebase-admin/data-connect';
 
 const connectorConfig = {
   connector: 'example',
   serviceId: 'vvp-site-code',
   location: 'us-east4'
 };
-exports.connectorConfig = connectorConfig;
+const _connectorConfig = connectorConfig;
+export { _connectorConfig as connectorConfig };
 
 function createUser(dcOrVarsOrOptions, varsOrOptions, options) {
   const { dc: dcInstance, vars: inputVars, options: inputOpts} = validateAdminArgs(connectorConfig, dcOrVarsOrOptions, varsOrOptions, options, true, true);
   dcInstance.useGen(true);
   return dcInstance.executeMutation('CreateUser', inputVars, inputOpts);
 }
-exports.createUser = createUser;
+const _createUser = createUser;
+export { _createUser as createUser };
 
 function getUserByEmail(dcOrVarsOrOptions, varsOrOptions, options) {
   const { dc: dcInstance, vars: inputVars, options: inputOpts} = validateAdminArgs(connectorConfig, dcOrVarsOrOptions, varsOrOptions, options, true, true);
   dcInstance.useGen(true);
   return dcInstance.executeQuery('GetUserByEmail', inputVars, inputOpts);
 }
-exports.getUserByEmail = getUserByEmail;
+const _getUserByEmail = getUserByEmail;
+export { _getUserByEmail as getUserByEmail };
 
 function createSession(dcOrVarsOrOptions, varsOrOptions, options) {
   const { dc: dcInstance, vars: inputVars, options: inputOpts} = validateAdminArgs(connectorConfig, dcOrVarsOrOptions, varsOrOptions, options, true, true);
   dcInstance.useGen(true);
   return dcInstance.executeMutation('CreateSession', inputVars, inputOpts);
 }
-exports.createSession = createSession;
+const _createSession = createSession;
+export { _createSession as createSession };
 
 function deleteSession(dcOrVarsOrOptions, varsOrOptions, options) {
   const { dc: dcInstance, vars: inputVars, options: inputOpts} = validateAdminArgs(connectorConfig, dcOrVarsOrOptions, varsOrOptions, options, true, true);
   dcInstance.useGen(true);
   return dcInstance.executeMutation('DeleteSession', inputVars, inputOpts);
 }
-exports.deleteSession = deleteSession;
+const _deleteSession = deleteSession;
+export { _deleteSession as deleteSession };
 
